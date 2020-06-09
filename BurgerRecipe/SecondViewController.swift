8c668fc1cef506255c77e4542e38b8c97c4bef6e
Bettina Carrizo <bgonzalez18@humana.com>
2020-06-09 09:54:41 -0300

    Initial Commit

---
+++ BurgerRecipe/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BurgerRecipe
+//
+//  Created by Bettina Carrizo on 6/9/20.
+//  Copyright © 2020 Bettina Carrizo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
